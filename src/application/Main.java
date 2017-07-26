d1db5a38148c4d9ce8911df869251d0f4d10e744
Michael <heymichaelw@gmail.com>
2017-07-26 09:40:20 -0400

    established connection

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,17 @@
 package application;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
 
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
+
+        } catch (SQLException ex) {
+            System.out.println("SQL Exception!");
+            ex.printStackTrace();
+        }
     }
 }
