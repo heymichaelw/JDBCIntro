fd494305f44b1c5087866142ce1b615779efa62f
Michael <heymichaelw@gmail.com>
2017-07-26 11:55:34 -0400

    set up user interface

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import application.models.Stat;
 
 import java.sql.*;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,30 +13,38 @@ public class Main {
         Class.forName("org.sqlite.JDBC");
 
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
-            DatabaseManager db = new DatabaseManager(connection);
-            db.dropStatsTable();
-            db.createStatsTable();
-            Statement statement = db.getStatement();
 
-            Stat tommyStat = new Stat("Tommy", 5, 8, statement);
-            Stat michaelStat = new Stat("Michael", 3, 6, statement);
-            tommyStat.save();
-            michaelStat.save();
-
-            List<Stat> results = Stat.findAll(db);
-            for (Stat stat : results){
-                stat.setWins(0);
-                stat.update();
-            }
-
-            results = Stat.findAll(db);
-            for (Stat stat: results){
-                System.out.println(stat);
-            }
+            WelcomeMenu();
 
         } catch (SQLException ex) {
             System.out.println("SQL Exception!");
             ex.printStackTrace();
         }
+    }
+
+    public static void WelcomeMenu() {
+        System.out.println("=====================================================");
+        System.out.println("Welcome to Stat Database!  What would you like to do?");
+        System.out.println("1.  Show All Stats");
+        System.out.println("2.  Add A New Stat");
+        System.out.println("3.  Update An Existing Stat");
+        System.out.println("=====================================================");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Now showing all stat data");
+                break;
+            case 2:
+                System.out.println("Let's add a stat");
+                break;
+            case 3:
+                System.out.println("Which entry are you updating?");
+                break;
+            default:
+                System.out.println("INVALID INPUT");
+        }
+
+        WelcomeMenu();
     }
 }
